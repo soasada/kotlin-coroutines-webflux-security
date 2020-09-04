d2cc4875465aca6622952137fbe998c74d025614
Nicolas Vargas Ortega <nvortega92@uma.es>
2020-09-04 08:49:26 +0200

    Update kill_java_process.sh

--- ci/kill_java_process.sh
+++ ci/kill_java_process.sh
@@ -1,2 +1,2 @@
-KOTLINAPPPID=$(/opt/prod_jdk/bin/jps -l | grep backend-server-.*.jar | awk '{print $1}')
+KOTLINAPPPID=$(/root/jdk/bin/jps -l | grep backend-server-.*.jar | awk '{print $1}')
 if [ -n "$KOTLINAPPPID" ]; then echo "Killing app with PID $KOTLINAPPPID" && kill -9 $KOTLINAPPPID; else echo 'App not running'; fi
